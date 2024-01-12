a6266679ac0cca432ae2fbf94b2e282a21fedc5f
Akash Jatav <akjatav128@gmail.com>
2024-01-12 07:16:01 +0530

    add_cart

--- store/views.py
+++ store/views.py
@@ -26,7 +26,6 @@ def store(request, category_slug = None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-
     except Exception as e: 
         raise e
     
